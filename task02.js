16897130da8fa6f11549228d0ac03928b89a8a63
nowrinksuda <maksudanowrin408@gmail.com>
2025-08-15 21:12:06 +0600

    first commit in array-task-js

---
+++ task02.js
@@ -0,0 +1,11 @@
+// 2. Add or remove elements
+// Declare an array of 3 tourist destinations
+// Add a new tourist destination to your tourist array
+// Add two more to your array
+// Remove the last tourist destination you have added
+// // display the final array as output
+const tourist_place = ['coxs bazar', 'sajek', 'saint-martain'];
+tourist_place.push('bandorbon');
+tourist_place.push('sylhet', 'rangamati');
+tourist_place.pop();
+console.log(tourist_place);
---
+++ tasko1.js
@@ -0,0 +1,9 @@
+// 1. Declare an array
+// Declare an array with 5 elements containing fruits
+// console log the 3rd index element
+// change the value of the 2nd index element to jambura
+// console log the final array
+const fruits = ['mango', 'apple', 'jackfruit', 'dragon', 'pineapple'];
+console.log(fruits[2]);
+fruits[1] = 'jambura';
+console.log(fruits);
